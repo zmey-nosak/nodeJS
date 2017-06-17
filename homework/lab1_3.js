34345a9a5cc5eb75b2ab74257b0ee0e8c5d0157f
zmey-nosak <zmey-nosak@yandex.ru>
2017-06-17 05:37:04 +0300

    add lab1

--- homework/lab1_3.js
+++ homework/lab1_3.js
@@ -5,12 +5,13 @@ var fileName2 = "body.html";
 var fileName3 = "footer.html";
 var http = require('http');
 http.createServer(function (req, res) {
-    // console.log('Server running on 8080');
-    // var files = [fileName1, fileName2, fileName3];
-    // var str="";
-    // for(var i=0;i<files.length;i++){
-    //     str+=fs.readFileSync(files[i],'utf-8').toString();
-    // }
-    // res.end(str);
+    console.log('Server running on 8080');
+    var files = [fileName1, fileName2, fileName3];
+    var str="";
+    for(var i=0;i<files.length;i++){
+        str+=fs.readFileSync(files[i],'utf-8').toString();
+    }
+    res.end(str);
+
 }).listen(8080);
 
