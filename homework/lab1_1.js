2234c0a3939c188a2578ad80cf3633ce06c4791f
Stepan.Koledov <stepan.koledov@billing.ru>
2017-06-27 15:28:05 +0300

    add nodejs homework2(site)

--- homework/lab1_1.js
+++ homework/lab1_1.js
@@ -1,7 +1,7 @@
 var http = require('http'); // подключение модуля
 
- var serv = http.createServer(function (request, response) {    // вызов метода создания http сервера
- console.log(request);
- console.log(response);
- });
- serv.listen(8080);
+var serv = http.createServer(function (request, response) {    // вызов метода создания http сервера
+    console.log(request);
+    console.log(response);
+});
+serv.listen(8080);
